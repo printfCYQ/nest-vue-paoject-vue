d8424307d0473eefadcd0bfcd96939ae86d9fa12
caiyongqing <#J29pJKhgn%>
2021-03-29 11:07:35 +0800

    文件上传

--- src/views/NewArticle.vue
+++ src/views/NewArticle.vue
@@ -31,6 +31,22 @@
         <el-input type="textarea" v-model="ruleForm.description"></el-input>
       </el-form-item>
       <el-form-item>
+        <el-upload
+          class="avatar-uploader"
+          action="http://localhost:3000/upload"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <img
+            v-if="ruleForm.imageUrl"
+            :src="ruleForm.imageUrl"
+            class="avatar"
+          />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item>
         <el-button
           v-if="id === ''"
           type="primary"
@@ -64,6 +80,7 @@ export default {
         name: "",
         kind: "",
         description: "",
+        imageUrl: "",
       },
       rules: {
         name: [
@@ -158,8 +175,26 @@ export default {
     },
     goBack() {
       this.id = "";
-      this.resetForm('ruleForm');
+      this.resetForm("ruleForm");
       this.$router.go(-1);
+    },
+
+    handleAvatarSuccess(res, file) {
+      console.log(res, file);
+      this.ruleForm.imageUrl = res.url;
+    },
+    beforeAvatarUpload(file) {
+      // const isJPG = file.type === 'image/jpeg';
+      // const isLt2M = file.size / 1024 / 1024 < 2;
+
+      // if (!isJPG) {
+      //   this.$message.error('上传头像图片只能是 JPG 格式!');
+      // }
+      // if (!isLt2M) {
+      //   this.$message.error('上传头像图片大小不能超过 2MB!');
+      // }
+      console.log(file);
+      // return isJPG && isLt2M;
     },
   },
 };
@@ -171,5 +206,29 @@ export default {
   .ruleForm {
     margin-top: 20px;
   }
+}
+
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
